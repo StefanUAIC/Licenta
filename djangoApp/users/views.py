a06d13f11dc131dbb236e726531d8bfc1f4633b8
Stefan <100302933+StefanUAIC@users.noreply.github.com>
2024-05-12 04:10:29 +0300

    first commit

---
+++ djangoApp/users/views.py
@@ -0,0 +1,27 @@
+from django.contrib.auth import authenticate
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .serializers import UserSerializer
+
+
+class UserLoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+        if user is None:
+            return Response({'error': 'Invalid login credentials'})
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key})
+
+
+class UserRegistrationView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
