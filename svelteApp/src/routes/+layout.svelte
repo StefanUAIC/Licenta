6766a905dbda08ff3e1f7999e4bd508dcb11ea4d
Stefan <scatiru1231@gmail.com>
2024-06-14 06:08:03 +0300

    some fixes on the appearance of notifications

--- svelteApp/src/routes/+layout.svelte
+++ svelteApp/src/routes/+layout.svelte
@@ -1,4 +1,3 @@
-<!-- src/routes/__layout.svelte -->
 <script lang="ts">
 	import '../app.css';
 	import { AppShell } from '@skeletonlabs/skeleton';
@@ -40,10 +39,10 @@
 <style>
     .hamburger {
         cursor: pointer;
-        padding: 10px;
+        padding: 1rem;
         position: fixed;
-        top: 10px;
-        left: 10px;
+        top: 1rem;
+        left: 1rem;
         z-index: 20;
     }
 
@@ -57,13 +56,13 @@
     }
 
     svg {
-        min-height: 24px;
+        min-height: 1.5rem;
         transition: transform 0.3s ease-in-out;
     }
 
     svg line {
         stroke: currentColor;
-        stroke-width: 3;
+        stroke-width: 0.1875rem;
         transition: transform 0.3s ease-in-out;
     }
 
@@ -72,7 +71,7 @@
     }
 
     .open #top {
-        transform: translate(6px, 0px) rotate(45deg);
+        transform: translate(0.375rem, 0) rotate(45deg);
         stroke: white;
     }
 
@@ -81,21 +80,21 @@
     }
 
     .open #bottom {
-        transform: translate(-12px, 9px) rotate(-45deg);
+        transform: translate(-0.75rem, 0.5625rem) rotate(-45deg);
         stroke: white;
     }
 
     .list-nav {
-        padding-top: 60px;
+        padding-top: 3.75rem;
     }
 
     .active-page::before {
         content: '';
         position: absolute;
-        left: -1rem;
+        left: -0.625rem;
         top: 0;
         bottom: 0;
-        width: 4px;
+        width: 0.25rem;
         background-color: white;
     }
 
@@ -124,94 +123,103 @@
 
     .notifications-button {
         cursor: pointer;
-        padding: 10px;
+        padding: 0.75rem;
         position: fixed;
-        top: 10px;
-        right: 10px;
+        top: 1rem;
+        right: 1rem;
         z-index: 20;
         background-color: white;
         border: none;
         border-radius: 50%;
-        width: 40px;
-        height: 40px;
+        width: 3rem;
+        height: 3rem;
         display: flex;
         justify-content: center;
         align-items: center;
     }
 
     .notifications-button svg {
-        width: 24px;
-        height: 24px;
+        width: 1.5rem;
+        height: 1.5rem;
         color: #4f46e5;
     }
 
     .notification-indicator {
         position: absolute;
-        top: 2px;
-        right: 2px;
-        width: 10px;
-        height: 10px;
+        top: 0;
+        left: 0;
+        width: 0.75rem;
+        height: 0.75rem;
         background-color: red;
         border-radius: 50%;
     }
 </style>
 
 <AppShell slotSidebarLeft="w-56 p-4">
-    {#if !['/', '/login', '/register'].includes(currentPage)}
-        <button class="hamburger" class:open={sidebarVisible} on:click={toggleSidebar}>
-            <svg width="32" height="24">
-                <line id="top" x1="0" y1="2" x2="32" y2="2" />
-                <line id="middle" x1="0" y1="12" x2="24" y2="12" />
-                <line id="bottom" x1="0" y1="22" x2="32" y2="22" />
-            </svg>
-        </button>
+	{#if !['/', '/login', '/register'].includes(currentPage)}
+		<button class="hamburger" class:open={sidebarVisible} on:click={toggleSidebar}>
+			<svg width="2rem" height="1.5rem">
+				<line id="top" x1="0" y1="0.125rem" x2="2rem" y2="0.125rem" />
+				<line id="middle" x1="0" y1="0.75rem" x2="1.5rem" y2="0.75rem" />
+				<line id="bottom" x1="0" y1="1.375rem" x2="2rem" y2="1.375rem" />
+			</svg>
+		</button>
 
-        <button class="notifications-button" on:click={toggleNotificationsModal}>
-            {#if $hasUnreadNotifications}
-                <div class="notification-indicator"></div>
-            {/if}
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.003 6.003 0 00-4-5.659V4a2 2 0 10-4 0v1.341C7.67 6.165 6 8.388 6 11v3.159c0 .417-.162.82-.451 1.13L4 17h11z" />
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 19a1 1 0 11-2 0h2z" />
-            </svg>
-        </button>
+		<button class="notifications-button" on:click={toggleNotificationsModal}>
+			{#if $hasUnreadNotifications}
+				<div class="notification-indicator"></div>
+			{/if}
+			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+					  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.003 6.003 0 00-4-5.659V4a2 2 0 10-4 0v1.341C7.67 6.165 6 8.388 6 11v3.159c0 .417-.162.82-.451 1.13L4 17h11z" />
+				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 19a1 1 0 11-2 0h2z" />
+			</svg>
+		</button>
 
-        <nav class="list-nav fixed top-0 left-0 w-56 h-full bg-indigo-600 z-10 p-4 sidebar {sidebarVisible ? 'visible' : ''}">
-            <ul>
-                <li class="relative mb-2">
-                    <a href="/posts" on:click={() => updateCurrentPage('/posts')}
-                       class={`nav-link block rounded-md px-4 py-2 ${currentPage === '/posts' ? 'active-page' : ''}`}>
-                        <i class="fas fa-blog mr-2"></i> Posts
-                    </a>
-                </li>
-                <li class="relative mb-2">
-                    <a href="/problems" on:click={() => updateCurrentPage('/problems')}
-                       class={`nav-link block rounded-md px-4 py-2 ${currentPage === '/problems' ? 'active-page' : ''}`}>
-                        <i class="fas fa-tasks mr-2"></i> Problems
-                    </a>
-                </li>
-                <li class="relative mb-2">
-                    <a href="/compiler" on:click={() => updateCurrentPage('/compiler')}
-                       class={`nav-link block rounded-md px-4 py-2 ${currentPage === '/compiler' ? 'active-page' : ''}`}>
-                        <i class="fas fa-code mr-2"></i> Compiler
-                    </a>
-                </li>
-                <li class="relative mb-2">
-                    <a href="/profile" on:click={() => updateCurrentPage('/profile')}
-                       class={`nav-link block rounded-md px-4 py-2 ${currentPage === '/profile' ? 'active-page' : ''}`}>
-                        <i class="fas fa-user mr-2"></i> Profile
-                    </a>
-                </li>
-            </ul>
-        </nav>
-    {/if}
+		<nav
+			class="list-nav fixed top-0 left-0 w-56 h-full bg-indigo-600 z-10 p-4 sidebar {sidebarVisible ? 'visible' : ''}">
+			<ul>
+				<li class="relative mb-2">
+					<a href="/posts" on:click={() => updateCurrentPage('/posts')}
+					   class={`nav-link block rounded-md px-4 py-2 ${currentPage === '/posts' ? 'active-page' : ''}`}>
+						<i class="fas fa-blog mr-2"></i> Posts
+					</a>
+				</li>
+				<li class="relative mb-2">
+					<a href="/problems" on:click={() => updateCurrentPage('/problems')}
+					   class={`nav-link block rounded-md px-4 py-2 ${currentPage === '/problems' ? 'active-page' : ''}`}>
+						<i class="fas fa-tasks mr-2"></i> Problems
+					</a>
+				</li>
+				<li class="relative mb-2">
+					<a href="/compiler" on:click={() => updateCurrentPage('/compiler')}
+					   class={`nav-link block rounded-md px-4 py-2 ${currentPage === '/compiler' ? 'active-page' : ''}`}>
+						<i class="fas fa-code mr-2"></i> Compiler
+					</a>
+				</li>
+				<li class="relative mb-2">
+					<a href="/profile" on:click={() => updateCurrentPage('/profile')}
+					   class={`nav-link block rounded-md px-4 py-2 ${currentPage === '/profile' ? 'active-page' : ''}`}>
+						<i class="fas fa-user mr-2"></i> Profile
+					</a>
+				</li>
+				<li class="relative mb-2">
+					<a href="/classes" on:click={() => updateCurrentPage('/classes')}
+					   class={`nav-link block rounded-md px-4 py-2 ${currentPage === '/classes' ? 'active-page' : ''}`}>
+						<i class="fas fa-users mr-2"></i> Classes
+					</a>
+				</li>
+			</ul>
+		</nav>
+	{/if}
 
-    <NotificationsModal bind:isOpen={$isNotificationsModalOpen} {hasUnreadNotifications} onClose={() => isNotificationsModalOpen.set(false)} />
+	<NotificationsModal bind:isOpen={$isNotificationsModalOpen} {hasUnreadNotifications}
+						onClose={() => isNotificationsModalOpen.set(false)} />
 
-    <div class="content-wrapper">
-        <main class="main-content">
-            <slot />
-        </main>
-        <Footer />
-    </div>
+	<div class="content-wrapper">
+		<main class="main-content">
+			<slot />
+		</main>
+		<Footer />
+	</div>
 </AppShell>
